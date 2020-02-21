b62ffd14942cea8acb5ce59a3416183a95f04771
Enric Mieza <emieza@xtec.cat>
2020-02-21 20:26:37 +0100

    database url

--- web/index.php
+++ web/index.php
@@ -12,7 +12,7 @@
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
 // Conectando y seleccionado la base de datos  
-$dbconn = pg_connect($dbopts)
+$dbconn = pg_connect("postgres://mmtupwnpdhmqnu:6c440c250c6c5c6af8d9e5dfdbf71e490f9ec6f5eb72142532ec3dbd52033b36@ec2-52-86-33-50.compute-1.amazonaws.com:5432/datk8i4decc3lm")
     or die('No se ha podido conectar: ' . pg_last_error());
 
 echo "CONNECTAT!";
