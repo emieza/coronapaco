3dcac810bbe932692b68376553a436ac797a3e91
Enric Mieza <emieza@xtec.cat>
2020-02-21 20:22:34 +0100

    db

--- web/index.php
+++ web/index.php
@@ -9,7 +9,17 @@
 <h2>Are you affected? Test here...</h2>
 
 <?php
-	phpinfo();
+
+$dbopts = parse_url(getenv('DATABASE_URL'));
+// Conectando y seleccionado la base de datos  
+$dbconn = pg_connect($dbopts)
+    or die('No se ha podido conectar: ' . pg_last_error());
+
+echo "CONNECTAT!";
+// Realizando una consulta SQL
+//$query = 'SELECT * FROM authors';
+//$result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
+
 ?>
 
 </body>
