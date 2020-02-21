c814b948b79cf5c601bcc1fe03b42dca2e44d863
Enric Mieza <emieza@xtec.cat>
2020-02-21 19:26:39 +0100

    primer

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<h1>CoronaPaco TEST</h1>
+<h2>Are you affected? Test here...</h2>
+
+<?php
+	phpinfo();
+?>
+
+</body>
+</html>
