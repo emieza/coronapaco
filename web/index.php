12840d3edfff2f17da2129ffdfea34ade08de030
Enric Mieza <emieza@xtec.cat>
2020-02-28 18:05:58 +0100

    connexio

--- web/index.php
+++ web/index.php
@@ -8,6 +8,15 @@
 <h1>CoronaPaco TEST</h1>
 <h2>Are you affected? Test here...</h2>
 
+
+<form method="post">
+
+	<input type="text" name="usuari">Usuari
+	<br>
+	<input type="password" name="contrasenya">Contrasenya
+	<br>
+	<input type="submit">
+
 <?php
 
 $dbopts = getenv('DATABASE_URL');
@@ -17,9 +26,23 @@ $dbconn = pg_connect($dbopts)
     or die('No se ha podido conectar: ' . pg_last_error());
 
 echo "CONNECTAT!";
+
+if( isset($_POST["usuari"]) ) {
+	$usuari = $_POST["usuari"];
+	$contrasenya = $_POST["contrasenya"];
+
+	echo "<p>Usuari:$usuari</p><p>Contrasenya:$contrasenya</p>";
+}
+
+
 // Realizando una consulta SQL
-//$query = 'SELECT * FROM authors';
+//$query = 'SELECT * FROM authors WHERE ';
 //$result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
+
+
+
+
+
 
 ?>
 
